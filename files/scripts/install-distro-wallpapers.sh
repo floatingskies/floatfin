060e13d36b236f54c8ace4d9d16c139f1aa88d4a
float <arielcloss@gmail.com>
2026-09-20 21:40:49 -0400

    feat: wallpapers from arkane linux, from fox to fox

--- files/scripts/install-distro-wallpapers.sh
+++ files/scripts/install-distro-wallpapers.sh
@@ -5,6 +5,6 @@ set -eou pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Register wallpaper collections with GNOME's wallpaper picker.
+"$SCRIPT_DIR/generate-gnome-wallpaper-xml.sh" /usr/share/backgrounds/crystalline_gravity crystalline_gravity "Crystalline Gravity"
 "$SCRIPT_DIR/generate-gnome-wallpaper-xml.sh" /usr/share/backgrounds/foxstruck foxstruck "Foxstruck"
-"$SCRIPT_DIR/generate-gnome-wallpaper-xml.sh" /usr/share/backgrounds/tree-of-life tree-of-life "Tree of Life"
-"$SCRIPT_DIR/generate-gnome-wallpaper-xml.sh" /usr/share/backgrounds/floating-continent floating-continent "Floating Continent"
+"$SCRIPT_DIR/generate-gnome-wallpaper-xml.sh" /usr/share/backgrounds/starstruck starstruck "Starstruck"
